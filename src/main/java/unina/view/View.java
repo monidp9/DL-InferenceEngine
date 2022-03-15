75f0b1fb7e86a68f07baf130b9a2631ab9943c88
Salvatore Amodio <sasi.amodio98@gmail.com>
2022-03-15 13:24:05 +0100

    reso pubblico metodo createPanel di ConceptPanel

--- src/main/java/unina/view/View.java
+++ src/main/java/unina/view/View.java
@@ -12,25 +12,20 @@ import java.awt.Desktop;
 
 public class View extends JFrame{
 
-    private ConceptPanel conceptPanel = null; 
-    private LoadingPanel loadingPanel = null; 
-    private int i = 0;
+    private LoadingPanel loadingPanel = null;
 
     public View(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public void openConceptReadingView(IOParser io) {
-        i = i+1;
-        this.conceptPanel = new ConceptPanel(io);
-        setContentPane(this.conceptPanel);
-        conceptPanel.createPanel();
+        setContentPane(new ConceptPanel(io));
         this.loadingPanel = new LoadingPanel();
+
     }
 
     public void openGraphView() {
         loadingPanel.closePanel();
-
         try {
             Desktop.getDesktop().open(new File("result/tableau_graph.png"));
         } catch(IOException e) {
